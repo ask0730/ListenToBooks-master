6632b7f3d8d98cbc7ccaeeeddf8cb02c5e53aec1
ask <2137576215@qq.com>
2025-05-07 17:09:59 +0800

    订单详情页面

--- pages/orderDetail/orderDetail.vue
+++ pages/orderDetail/orderDetail.vue
@@ -1,12 +1,12 @@
 <template>
 	<gui-page>
 		<template v-slot:gBody>
-			<view class="gui-relative">
+			<view class="gui-relative" style="background-color: pink;">
 				<view class="gui-list gui-padding gui-bg-orange gui-p-b-80 gui-p-t-80">
 					<text class="gui-h3 gui-color-white gui-block gui-p-l-30 gui-p-t-30 gui-p-b-50">{{ orderInfo?.orderStatusName }}</text>
 				</view>
 
-				<view class="gui-absolute-lb gui-bg-white gui-creation-live">
+				<view class="gui-absolute-lb gui-creation-live" style="background-color: pink;">
 
 					<scroll-view scroll-y style="height: 620rpx; width:670rpx;" class="gui-p-10">
 					  <view class="gui-flex gui-align-items-center gui-m-r-50 gui-m-b-20" v-for="item in orderInfo?.orderDetailList" :key="item.id">
@@ -16,37 +16,37 @@
 					  		:src="item?.itemUrl"
 					  	></image>
 					  	<view class="gui-flex gui-column gui-flex1 gui-m-l-20">
-					  		<text class="gui-block gui-ellipsis-line2 gui-h5">{{ item?.itemName }} ￥{{ item?.itemPrice }}</text>
+					  		<text class="gui-block gui-ellipsis-line2 gui-h5 gui-color-white">{{ item?.itemName }} ￥{{ item?.itemPrice }}</text>
 					  	</view>
 					  </view>
 					</scroll-view>
 
 					<view class="gui-flex gui-space-between gui-align-items-end gui-m-t-10">
-						<text>商品总价</text>
-						<text class="gui-block gui-color-gray">
+						<text class="gui-color-white">商品总价</text>
+						<text class="gui-block gui-color-white">
 							<text>￥{{ orderInfo?.originalAmount }}</text>
 						</text>
 					</view>
 					<view class="gui-border-b gui-m-t-20 gui-m-b-20"></view>
 					<view class="gui-flex gui-space-between gui-align-items-end gui-m-t-10">
-						<text>实付款</text>
-						<text class="gui-block gui-color-gray">
+						<text class="gui-color-white">实付款</text>
+						<text class="gui-block gui-color-white">
 							<text class="gui-text-small gui-color-red gui-h5 gui-bold">￥{{ orderInfo?.orderAmount }}</text>
 						</text>
 					</view>
 					<view class="gui-border-b gui-m-t-20 gui-m-b-40"></view>
 
 					<view class="gui-flex gui-space-between gui-align-items-end gui-m-t-10">
-						<view>订单号</view>
+						<view class="gui-color-white">订单号</view>
 						<br>
-						<view class="gui-block gui-color-gray">
+						<view class="gui-block gui-color-white">
 							<text>{{ orderInfo?.orderNo }}</text>
 						</view>
 					</view>
 					<view class="gui-border-b gui-m-t-20 gui-m-b-20"></view>
 					<view class="gui-flex gui-space-between gui-align-items-end gui-m-t-10">
-						<text>付款方式</text>
-						<text class="gui-block gui-color-gray">
+						<text class="gui-color-white">付款方式</text>
+						<text class="gui-block gui-color-white">
 							<text>{{ orderInfo?.payWayName }}</text>
 						</text>
 					</view>
