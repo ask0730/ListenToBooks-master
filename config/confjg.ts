962fe92763057edcdc76d0de364c0d4c63660c6f
ask <2137576215@qq.com>
2025-06-07 03:50:35 +0800

    vd367488

--- config/confjg.ts
+++ config/confjg.ts
@@ -5,8 +5,8 @@
 // // webSocket基础路径
 // export const WebSocket_BASE_URL = "ws://139.198.163.91:8500/api/websocket"
 // 基础请求路径
-export const BASE_URL = "http://127.0.0.1:8500"
+export const BASE_URL = "http://vd367488.natappfree.cc"
 // 基础上传路径
-export const BASE_UPLOAD_URL = "http://127.0.0.1:8500"
+export const BASE_UPLOAD_URL = "http://vd367488.natappfree.cc"
 // webSocket基础路径
 export const WebSocket_BASE_URL = "ws://192.168.200.0:8500/api/websocket"
